0ad3ffbedcae7081734d5f342b6d0393dca7f871
tamiresv19santos-create <tamiresv19santos@gmail.com>
2025-10-01 16:55:46 -0300

    Add files via upload

--- selects_filmes.sql
+++ selects_filmes.sql
@@ -37,27 +37,44 @@ ORDER BY duracao_minutos DESC
 LIMIT 10;
 
 -- 6. Eu quero saber quais são os filmes do Diretor Christopher Nolan
-
+SELECT titulo, diretor FROM filmes where diretor = "Christopher Nolan"
+SELECT * FROM filmes where diretor = "Christopher Nolan"
 -- 7. Eu quero saber quais são os filmes do James Gunn
-
+SELECT titulo, diretor FROM filmes where diretor = "James Gunn"
+SELECT * FROM filmes where diretor = "James Gunn"
 -- 8. Eu quero saber quais os filmes de ação lançados em 2016
+SELECT titulo, ano_lancamento FROM filmes where ano_lancamento  = 2016 and genero = "Ação"
 
 -- 9. Eu quero saber quais os filmes do batman e quais os diretores
+SELECT titulo, diretor from filmes where titulo like "%batman%"
 
 -- 10. Eu quero saber quais os filmes de terror e drama lançado entre 1980 e 2002
-
+SELECT titulo, genero , ano_lancamento
+from filmes
+where (genero = "Drama" or genero = "Terror")
+and ano_lancamento >=1980 and ano_lancamento <=2002
+order by ano_lancamento ASC
 -- 11. Eu quero Saber quantos filmes foram lançados em 1999
+SELECT titulo, ano_lancamento from filmes where ano_lancamento = 1999
+SELECT count(*) as total from filmes where ano_lancamento = 1999
 
 -- 12. Eu quero saber quantos filmes tem o diretor Quentin Tarantino
+SELECT titulo, diretor FROM filmes where diretor = "Quentin Tarantino"
+SELECT count(*) as total FROM filmes where diretor = "Quentin Tarantino"
 
 -- 13. Eu quero saber quais os filmes do James Cameron e quais os generos deles
+SELECT titulo, diretor, genero from filmes where diretor = "James Cameron"
 
 -- 14. Por fim vamos adiconar as informações do seu filme favorito na tabela.
+INSERT INTO filmes (titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb)VALUES
+('Como eu era antes de você', 'Romance/Drama', 2016,110, 'Thea Sharrock', 4.8)
 
 -- 15. Vamos adiconar as informações do filme que o professor odeia.
-
+INSERT INTO filmes (titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb)VALUES
+('Homem - Aranha: Longe de casa', 'Ação', 2019,130, 'Jon Watts', 7.4)
 -- 16. Vamos deletar esse filme que o professor detesta.
+DELETE from filmes where id = 203
 
-
+SELECT * from filmes order by id desc
 
 
